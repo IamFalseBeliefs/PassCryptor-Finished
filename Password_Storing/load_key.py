ce74339467ca7e98282d2cf831cecc92d5a5b25e
Charles Steven Whitcomb JR <64711936+IamFalseBielfs@users.noreply.github.com>
2023-03-02 00:05:25 -0500

    Delete Password_Storing directory

--- Password_Storing/load_key.py
+++
@@ -1,6 +0,0 @@
-# // Imports \\ #
-from cryptography.fernet import Fernet
-
-# // Load Key Function \\ #
-def load_key():
-	return open("./program_information/key.key", 'rb').read()
--- Password_Storing/load_path.py
+++
@@ -1,6 +0,0 @@
-# // Imports \\ #
-from cryptography.fernet import Fernet
-
-def load_path():
-
-	return open("./program_information/usr_path.enc", 'r').read()
